04a5537c65cb0b172db249a6935cc851630159be
Pramuditha Kaushalya <pramuditha.kaushalya@gmail.com>
2019-10-11 17:41:29 +0530

    Stop Watch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Wrestling Score Board" />
   </div>
 </template>
 
@@ -16,13 +16,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,137 @@
 <template>
+
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <h1> {{ msg }}</h1>
+    </nav>
+    <br><br><br>
+    <center>
+      <div style="background:#ECECEC; padding:50px; padding-right:100px">
+        <div class="card bg-light mb-3 col-12" style="max-width: 20rem;">
+          <div class="card-body">
+            <div class="column">
+            <h2>
+              {{ minutes | zeroPad }} :
+              {{ seconds | zeroPad }} :
+              {{ milliSeconds | zeroPad(3) }}
+            </h2>
+            </div>
+          </div>
+        </div>
+        <div class="card bg-light mb-3 col-12" style="max-width: 20rem;">
+          <div class="card-body">
+            <div class="column">
+            <h2>
+              {{ seconds | zeroPad }} :
+              {{ milliSeconds | zeroPad(3) }}
+            </h2>
+            </div>
+          </div>
+        </div>
+     </div>
+    </center>
+   
+      <button type="button" class="btn btn-secondary" @click="startTimer" :disabled="isRunning">Start</button>
+      <button type="button" class="btn btn-secondary" @click="stopTimer" :disabled="!isRunning">Stop</button>     
+      <button type="button" class="btn btn-secondary" @click="clearAll">Restart</button>
+      <button type="button" class="btn btn-primary">Passive</button>
+      <button type="button" class="btn btn-danger">Passive</button>
+      <!-- <div class="alert alert-dismissible alert-danger">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>Red is passive!</strong> <br> Start active period.
+      </div> -->
   </div>
 </template>
 
-<script>
+<script type="text/babel">
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: 'HelloWorld',
+
+    props: {
+      msg: String
+    },
+
+    components: {
+      
+    },
+
+    data() {
+      return {
+        animateFrame: 0,
+        nowTime: 0,
+        diffTime: 0,
+        startTime: 0,
+        isRunning: false
+      };
+    },
+
+    methods: {
+       setSubtractStartTime: function (time) {
+        typeof time !== 'undefined' ? time : 0;
+        this.startTime = Math.floor(performance.now() - time);
+      },
+   
+      startTimer: function () {
+
+        var vm = this;
+        vm.setSubtractStartTime(vm.diffTime);
+
+        (function loop(){
+          vm.nowTime = Math.floor(performance.now());
+          vm.diffTime = vm.nowTime - vm.startTime;
+          vm.animateFrame = requestAnimationFrame(loop);
+        }());
+        vm.isRunning = true;
+      },
+
+      stopTimer: function () {
+        this.isRunning = false;
+        cancelAnimationFrame(this.animateFrame);
+      },
+
+      clearAll: function () {
+        this.startTime = 0;
+        this.nowTime = 0;
+        this.diffTime = 0;
+        this.stopTimer();
+        this.animateFrame = 0;
+      }
+    },
+
+    computed: {
+      minutes: function () {
+        return Math.floor(this.diffTime / 1000 / 60) % 60;
+      },
+
+      seconds: function () {
+        return Math.floor(this.diffTime / 1000) % 60;
+      },
+
+      milliSeconds: function () {
+        return Math.floor(this.diffTime % 1000);
+      }
+    },
+
+    filters: {
+      
+      zeroPad: function(value, num){
+        typeof num !== 'undefined' ? num : 2;
+        return value.toString().padStart(num,"0");
+      }
+    }    
+  }    
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+h1 {
+  color: #ADD8E6;
+  border-color:  #000;
+  margin-left: 600px;
+}
 h3 {
   margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
